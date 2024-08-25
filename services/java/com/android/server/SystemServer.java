edfcaa0901dfd39619e023439be8688bb632ebff
Ido Ben-Hur <idoybh2@gmail.com>
2024-08-25 14:58:21 +0300

    base: Make PowerOffAlarmService opt-in
    Change-Id: I17343b10420907f495e5b35d5bf80a278f66dbb5

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1752,9 +1752,11 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(LogcatManagerService.class);
             t.traceEnd();
 
-            t.traceBegin("StartPowerOffAlarmService");
-            mSystemServiceManager.startService(PowerOffAlarmService.class);
-            t.traceEnd();
+            if (context.getResources().getBoolean(R.bool.config_powerOffAlarmEnabled)) {
+                t.traceBegin("StartPowerOffAlarmService");
+                mSystemServiceManager.startService(PowerOffAlarmService.class);
+                t.traceEnd();
+            }
 
             t.traceBegin("StartSmart5gService");
             mSystemServiceManager.startService(Smart5gService.class);
